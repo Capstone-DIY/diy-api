e8649c29534f728903f393e96016433d3914b0c4
Satria <satriasauraly@gmail.com>
2024-11-21 01:01:32 +0700

    Writing in diary and updating server.js

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 
+// User routes
 const { createAuthHandler } = require('./src/app/auth.js');
 const { createUserHandler, getUserByIdHandler, deleteUserByIdHandler } = require('./src/app/user.js');
+
+// Diary routes
+const { createDiaryHandler, getAllDiaryHandler } = require('./src/app/diary.js');
 
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
@@ -14,14 +18,16 @@ app.use(cors());
 app.use(express.json());
 
 // Auth routes
-app.post('/login', createAuthHandler);
+// app.post('/login', createAuthHandler);
 
 // User routes
-app.post('/register', createUserHandler);
-app.get('/users/:userId', getUserByIdHandler);
-app.delete('/users/:userId', deleteUserByIdHandler);
+// app.post('/register', createUserHandler);
+// app.get('/users/:userId', getUserByIdHandler);
+// app.delete('/users/:userId', deleteUserByIdHandler);
 
 // Diary routes
+app.post('/diary/create', createDiaryHandler);
+app.get('/diary', getAllDiaryHandler);
 
 app.use((err, req, res, next) => {
   console.error(err);
--- src/app/diary.js
+++ src/app/diary.js
@@ -1,1 +1,35 @@
 // TODO: Complete this by tomorrow!
+const uuid  = require('uuid');
+
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+async function createDiaryHandler(req, res, next) {
+  const payload = req.body;
+  try {
+    const newDiary = await prisma.diary.create({
+      data: {
+        diaryId: uuid(),
+        date: new Date(),
+        title: payload.title,
+        story: payload.story,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    });
+    return res.status(200).json({message: 'Diary berhasil ditambahkan'});
+  } catch (err) {
+    return next(err);
+  }
+};
+
+async function getAllDiaryHandler(req, res, next) {
+  try {
+    const diaries = await prisma.diary.findMany();
+    return res.status(200).json({data: diaries});
+  } catch (err) {
+    return next(err);
+  }
+};
+
+module.exports = { createDiaryHandler, getAllDiaryHandler };
