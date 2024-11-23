fe723281fefc5eac7b7c58f3dc9299ada9b9d48f
Sat-Lab <LABKOM@DESKTOP-0I6DHSS>
2024-11-23 09:14:43 +0700

    Adding date-fns to format user date input

--- src/app/diary.js
+++ src/app/diary.js
@@ -1,4 +1,5 @@
 const { authenticateJWT } = require('../middleware.js');
+const { parse } = require('date-fns'); 
 
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
@@ -8,8 +9,9 @@ const router = express.Router();
 
 // Membuat diary baru
 router.post('/create', authenticateJWT, async (req, res, next) => {
-  const payload = req.body;
   const userId = req.userId; // Mengambil userId dari request object yang sudah di-decode dari token
+
+  const payload = req.body;
 
   if (!payload.date || !payload.title || !payload.story) {
     return res.status(400).json({
@@ -19,13 +21,39 @@ router.post('/create', authenticateJWT, async (req, res, next) => {
   }
 
   try {
+
+    const parseDate = (dateString) => {
+      // Format yang bisa diterima
+      const formats = [
+        'EEEE, dd - MMMM - yyyy',
+        'EEEE, dd MMMM yyyy',
+        'dd-MM-yyyy',
+        'dd MM yyyy',
+        'dd/MM/yyyy',
+        'yyyy-MM-dd'
+      ];
+
+      for(let formatSting of formats) {
+        const parsedDate = parse(dateString, formatSting, new Date());
+        if(!isNaN(parsedDate)) {
+          // Ubah format yang bisa diterima PostgreSQL
+          return format(parsedDate, 'yyyy-MM-dd');
+        }
+      };
+
+      return null;
+    };
+
     // Membuat diary untuk user yang sedang login
     const newDiary = await prisma.diary.create({
       data: {   
-        date: payload.date,
+        date: parseDate(payload.date),
         title: payload.title,
         story: payload.story,
-        emotion: payload.emotion, // Optional
+        
+        // TODO: Ambil emotion dari prediksi model
+        emotion: payload.emotion,
+
         created_at: new Date(),
         updated_at: new Date(),
         userId: userId,  // Menyertakan userId yang didapat dari token
@@ -46,34 +74,15 @@ router.post('/create', authenticateJWT, async (req, res, next) => {
   }
 });
 
-// Menampilkan semua diary berdasarkan user id
-router.get('/:userId', authenticateJWT, async (req, res, next) => {
-  const id = req.params.id;  // Mengambil id user dari params
-
-  try {
-    const diaries = await prisma.diary.findMany({
-      where: { userId: parseInt(id) },
-    });
-
-    return res.status(200).json({
-      status_code: 200,
-      message: 'Diary berhasil ditemukan',
-      data: diaries,
-    });
-  } catch (err) {
-    return next(err);
-  }
-});
-
 // Menampilkan diary berdasarkan id diary
 router.get('/:diaryId', authenticateJWT, async (req, res, next) => {
   const id = req.params.id;  // Mengambil id diary dari params
-
+  
   try {
     const diary = await prisma.diary.findUnique({
       where: { id: parseInt(id) },
     });
-
+    
     return res.status(200).json({
       status_code: 200,
       message: 'Diary berhasil ditemukan',  
@@ -130,6 +139,25 @@ router.delete('/:diaryId', authenticateJWT, async (req, res, next) => {
       status_code: 200,
       message: 'Diary berhasil dihapus',
       data: deletedDiary,
+    });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+// Menampilkan semua diary berdasarkan user id
+router.get('/:userId', authenticateJWT, async (req, res, next) => {
+  const id = req.params.id;  // Mengambil id user dari params
+
+  try {
+    const diaries = await prisma.diary.findMany({
+      where: { userId: parseInt(id) },
+    });
+
+    return res.status(200).json({
+      status_code: 200,
+      message: 'Diary berhasil ditemukan',
+      data: diaries,
     });
   } catch (err) {
     return next(err);
