fc35cd1d3e628b35a5a677b0233b9a624f767d2a
Satria <satriasauraly@gmail.com>
2024-12-02 20:08:40 +0700

    Editing firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,9 +1,10 @@
 const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
 const firebase = require('firebase-admin');
 
-// Initialize secret manager client
+// Initialize Secret Manager client
 const client = new SecretManagerServiceClient();
 
+// Function to retrieve a secret from Google Secret Manager
 async function getSecret(secretName) {
   try {
     const [version] = await client.accessSecretVersion({
@@ -11,29 +12,35 @@ async function getSecret(secretName) {
     });
     
     const payload = version.payload.data.toString('utf8');
-    return JSON.parse(payload); // Mengembalikan Service Account JSON
+    return JSON.parse(payload); // Return the Service Account JSON
   } catch (error) {
     console.error('Error retrieving secret:', error);
     throw new Error('Error retrieving secret');
   }
 }
 
-
+// Function to initialize Firebase
 async function initializeFirebase() {
   try {
     const serviceAccount = await getSecret('firebase-service-account');
 
-    firebase.initializeApp({
-      credential: firebase.credential.cert(serviceAccount),
-    });
+    // Initialize Firebase only if it hasn't been initialized already
+    if (!firebase.apps.length) {
+      firebase.initializeApp({
+        credential: firebase.credential.cert(serviceAccount),
+      });
 
-    console.log('Firebase initialized');
+      console.log('Firebase initialized');
+    } else {
+      console.log('Firebase already initialized');
+    }
   } catch (error) {
     console.error('Error initializing Firebase:', error);
+    throw new Error('Firebase initialization failed');
   }
 }
 
-// Memastikan Firebase terinisialisasi sebelum aplikasi berjalan
+// Ensure Firebase is initialized before app starts
 (async () => {
   await initializeFirebase();
 })();
